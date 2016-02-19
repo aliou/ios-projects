126586597d3b64e1446ad3d1ea466c990e33822d
Aliou Diallo <code@aliou.me>
2016-02-19 01:24:03 +0100

    Improve code.

--- 01 - Tap Counter/Tap Counter/ViewController.swift
+++ 01 - Tap Counter/Tap Counter/ViewController.swift
@@ -10,36 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
-  var value = 0
-  @IBOutlet weak var valueLabel: UILabel!
+  var counter = 0 {
+    didSet {
+      valueLabel.text = String(counter)
+    }
+  }
+
+  @IBOutlet weak var valueLabel: UILabel! {
+    didSet {
+      valueLabel.text = "0"
+    }
+  }
+
+  @IBOutlet weak var resetButton: UIBarButtonItem! {
+    didSet {
+      resetButton.title = "Reset"
+    }
+  }
 
   override func viewDidLoad() {
     super.viewDidLoad()
-
-    let leftButton =  UIBarButtonItem(title: "Reset", style: .Plain, target: self, action: "reset")
-
-    navigationItem.leftBarButtonItem = leftButton
     navigationItem.title = "Counter"
-
-    reset()
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
   }
 
-  @IBAction func updateLabel(sender: AnyObject) {
-    self.value += 1
-    updateLabel()
+  @IBAction func resetCounter(sender: AnyObject) {
+    self.counter = 0
   }
 
-  func reset() {
-    self.value = 0
-    updateLabel()
-  }
-
-  func updateLabel() {
-    valueLabel.text = String(value)
+  @IBAction func increaseCounter(sender: AnyObject) {
+    self.counter++
   }
 }
 
---
+++ 01 - Tap Counter/readme.md
@@ -0,0 +1,37 @@
+### Notes
+
+#### Updating a something on value change
+
+In our case, we want to update the label when the counter value changes. To do
+this, you can use the `didSet` observer on our counter:
+
+```swift
+var counter = 0 {
+  didSet {
+    // Whatever I'm updating.
+  }
+}
+```
+
+
+#### Casting the counter value to String
+
+To set the integer counter value in our label, we can directly use the `String`
+constructor:
+
+```swift
+valueLabel.text = String(counter)
+```
+
+#### Initial styling IBoulets
+
+Instead of adding the styling of IBoulets in the `viewDidLoad` method, we can
+add it to the `didSet` observer:
+
+```swift
+@IBOutlet weak var resetButton: UIBarButtonItem! {
+  didSet {
+    resetButton.title = "Reset"
+  }
+}
+```
