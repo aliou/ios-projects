c2c73320ac7f6ea02fa95dc78c684075943a071e
Aliou Diallo <code@aliou.me>
2016-02-25 01:36:13 +0100

    Create ListDisplayable protocol.

---
+++ 05 - Image Feed/Image Feed/Protocols/ListDisplayable.swift
@@ -0,0 +1,11 @@
+//
+//  ListDisplayable.swift
+//  Image Feed
+//
+//  Created by Aliou Diallo on 25/02/16.
+//  Copyright © 2016 Aliou Diallo. All rights reserved.
+//
+
+protocol ListDisplayable {
+  var label: String { get set }
+}
