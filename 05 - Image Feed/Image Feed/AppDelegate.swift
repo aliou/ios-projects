806b204171ffaa6a8dac97744c263f250021557e
Aliou Diallo <code@aliou.me>
2016-03-08 06:56:33 +0100

    Manually add images.

--- 05 - Image Feed/Image Feed/AppDelegate.swift
+++ 05 - Image Feed/Image Feed/AppDelegate.swift
@@ -15,7 +15,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject : AnyObject]?) -> Bool {
     if let window = window {
-      window.rootViewController = NavigationController(rootViewController: ImageListViewController<Image>())
+      let rvc = ImageListViewController<Image>()
+      rvc.items = [
+        Image(label: "Buildings", imageURL: "https://source.unsplash.com/category/buildings"),
+        Image(label: "Food", imageURL: "https://source.unsplash.com/category/food"),
+        Image(label: "Nature", imageURL: "https://source.unsplash.com/category/nature"),
+        Image(label: "People", imageURL: "https://source.unsplash.com/category/people"),
+        Image(label: "Technology", imageURL: "https://source.unsplash.com/category/technology"),
+        Image(label: "Objects", imageURL: "https://source.unsplash.com/category/objects"),
+      ]
+      window.rootViewController = NavigationController(rootViewController: rvc)
       window.makeKeyAndVisible()
     }
 
