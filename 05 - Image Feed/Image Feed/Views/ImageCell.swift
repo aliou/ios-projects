bbe6cb838b61ab7322af3d1bc210878e711dd747
Aliou Diallo <code@aliou.me>
2016-03-08 06:50:40 +0100

    Add initializers for the custom cell.

--- 05 - Image Feed/Image Feed/Views/ImageCell.swift
+++ 05 - Image Feed/Image Feed/Views/ImageCell.swift
@@ -9,4 +9,12 @@
 import UIKit
 
 class ImageCell: UITableViewCell {
+
+  override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+  }
+
+  required init?(coder aDecoder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
 }
