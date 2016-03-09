f39cea2820b8a85f2a7c8108ebf934856b5c8614
Aliou Diallo <code@aliou.me>
2016-03-09 19:34:27 +0100

    Download image in a background queue.

--- 05 - Image Feed/Image Feed/Models/Image.swift
+++ 05 - Image Feed/Image Feed/Models/Image.swift
@@ -12,8 +12,27 @@ struct Image: ListDisplayable, RemoteResourceable {
   var label: String
   var resourceURL: NSURL?
 
+  var image: UIImage?
+
   init(label: String, imageURL: String) {
     self.label = label
     self.resourceURL = NSURL(string: imageURL)
+
+    downloadResource()
+  }
+
+  mutating func downloadResource() {
+    let queue = NSOperationQueue()
+
+    queue.addOperationWithBlock() {
+      if let _ = self.image { return }
+
+      if let resource = self.resourceURL {
+        let imageData = NSData(contentsOfURL: resource)
+        if let imageData = imageData {
+          self.image = UIImage(data: imageData)
+        }
+      }
+    }
   }
 }
