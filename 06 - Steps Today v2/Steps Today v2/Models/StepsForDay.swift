df86d46d4aa265b1c5b6beca6448e0c242af835e
Aliou Diallo <code@aliou.me>
2016-03-17 19:40:57 +0100

    Initial steps fetching.

--- 06 - Steps Today v2/Steps Today v2/Models/StepsForDay.swift
+++ 06 - Steps Today v2/Steps Today v2/Models/StepsForDay.swift
@@ -7,18 +7,28 @@
 //
 
 import UIKit
+import CoreMotion
 
 struct StepsForDay {
+  static let pedometer = CMPedometer()
+
   let date: NSDate
-  let steps: Int
+  var steps: Int?
 
   init(date: NSDate) {
-    self.date = date
+    self.date = date.beginningOfDay()
     // Get the date from the podometer.
-    self.steps = 0
+    StepsForDay.pedometer.queryPedometerDataFromDate(self.date, toDate: self.date.endOfDay()) {
+      (data: CMPedometerData?, error: NSError?) -> Void in
+      if let data = data {
+        self.steps = Int(data.numberOfSteps)
+      } else {
+        self.steps = nil
+      }
+    }
   }
 
-  func currentDate() -> Bool {
-    return false
+  func isCurrentDate() -> Bool {
+    return self.date.beginningOfDay() == NSDate().beginningOfDay()
   }
 }
