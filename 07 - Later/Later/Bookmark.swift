a6ccfbc11adc6ff91d821f3edcc0ce6a31d2c4e8
Aliou Diallo <code@aliou.me>
2016-06-22 20:50:33 +0200

    Add Bookmark model.

---
+++ 07 - Later/Later/Bookmark.swift
@@ -0,0 +1,23 @@
+//
+//  Bookmark.swift
+//  Later
+//
+//  Created by Aliou Diallo on 22/06/16.
+//  Copyright © 2016 Aliou Diallo. All rights reserved.
+//
+
+import UIKit
+
+struct Bookmark {
+  let date: NSDate
+  let url: NSURL
+  let title: String?
+  let description: String?
+
+  init(date: NSDate, url: String, title: String?, description: String?) {
+    self.date = date
+    self.url = NSURL(string: url)!
+    self.title = title
+    self.description = description
+  }
+}
